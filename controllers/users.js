3e7830d7372c3073399a7ef059275b05af0d3fa2
deev <champdeev@gmail.com>
2024-04-27 10:31:50 +0530

    controllers added

---
+++ controllers/users.js
@@ -0,0 +1,62 @@
+const User = require("../models/usermodel");
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    console.log(user);
+    await user.save();
+    res.status(201).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+// Get user by ID
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+// Update user by ID
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+// Delete user by ID
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,65 +1,21 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/usermodel')
+
+const user_controller = require('../controllers/users')
 
 // Create a new user
-router.post('/', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).send(user);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
+router.post('/', user_controller.createUser);
 
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.get('/', user_controller.getAllUsers)
 
 // Get user by ID
-router.get('/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.get('/:id', user_controller.getUserById);
 
 // Update user by ID
-router.patch('/:id', async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
+router.patch('/:id', user_controller.updateUser);
 
 // Delete user by ID
-router.delete('/:id', async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.delete('/:id', user_controller.deleteUser);
 
 
 module.exports = router;
